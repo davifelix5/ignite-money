95d74183f53ae61d372d9bd183950c82d8de71a2
Davi F <davi.felix.lima@gmail.com>
2022-02-07 20:55:25 -0300

    feature: add miragejs

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,8 +1,17 @@
+import { useEffect } from "react";
 import { Container } from "./styles";
 import { Summary } from "../Summary";
 import { TransactionsTable } from "../TrasactionsTable";
+import { api } from "../../services/api";
 
 export function Dashboard() {
+
+  useEffect(() => {
+    api.get('transactions').then(response => {
+      console.log(response.data)
+    })
+  }, [])
+
   return (
     <Container>
       <Summary />
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs'
 import App from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api'
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1, 
+          title: 'Salario', 
+          amount: 5000, 
+          type: 'income', 
+          category: 'Trabalho', 
+          description: 'Salário mensal'
+        },
+        {
+          id: 2, 
+          title: 'Almoço', 
+          amount: 30, 
+          type: 'outcome', 
+          category: 'Alimenção', 
+          description: 'Almoço fora'
+        },
+        {
+          id: 3, 
+          title: 'Aluguel', 
+          amount: 1500, 
+          type: 'outcome', 
+          category: 'Despesa fixa', 
+          description: 'Pagamento do aluguel'
+        },
+      ]
+    })    
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
