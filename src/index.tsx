9feeffb6fafb6e11679ba87a3073c4ec58351eb3
Davi F <davi.felix.lima@gmail.com>
2022-02-08 12:20:38 -0300

    fix: separete mirage server to another file

--- src/index.tsx
+++ src/index.tsx
@@ -1,74 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Modal from 'react-modal'
-import { createServer, Model, Response } from 'miragejs'
 import App from './App';
 
+import './mirageServer'
+
 Modal.setAppElement('#root')
-
-createServer({
-  models: {
-    transaction: Model
-  },
-
-  seeds(server) {
-    server.db.loadData({
-      transactions: [
-        {
-          id: 1, 
-          title: 'Salario', 
-          value: 5000, 
-          type: 'income', 
-          category: 'Trabalho', 
-          createdAt: new Date('2022-02-06 10:20:00')
-        },
-        {
-          id: 2, 
-          title: 'Almoço', 
-          value: 30, 
-          type: 'outcome', 
-          category: 'Alimenção', 
-          createdAt: new Date('2022-02-05 19:40')
-        },
-        {
-          id: 3, 
-          title: 'Aluguel', 
-          value: 1500, 
-          type: 'outcome', 
-          category: 'Despesa fixa', 
-          createdAt: new Date('2022-02-04 18:20')
-        },
-      ]
-    })
-  },
-
-  routes() {
-    this.namespace = 'api'
-
-    this.get('/transactions', () => {
-      return this.schema.all('transaction')
-    })
-    
-    this.post('/transactions', (schema, request) => {
-      const data = JSON.parse(request.requestBody)
-      return schema.create('transaction', {
-        ...data,
-        createdAt: new Date()
-      })
-    })
-
-    this.delete('/transactions/:id', (schema, request) => {
-      const { id } = request.params
-      const transaction = schema.find('transaction', id)
-      if(!transaction) {
-        return new Response(404)
-      }
-      transaction.destroy()
-      return new Response(204)
-      
-    })
-  }
-})
 
 ReactDOM.render(
   <React.StrictMode>
