2d13855c1d93bc2bb19984b07bac8b4649665c8c
Davi F <davi.felix.lima@gmail.com>
2022-02-08 09:58:33 -0300

    feature: calculate summary data

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,34 +1,62 @@
+import { useTrasactions } from "../../hooks/useTransactions";
 import { Container } from "./styles";
 
-import income from '../../assets/income.svg'
-import outcome from '../../assets/outcome.svg'
-import total from '../../assets/total.svg'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import totalImg from '../../assets/total.svg'
+
+type Type = 'income' | 'outcome'
 
 export function Summary() {
+
+  const { transactions } = useTrasactions()
+
+  function calculateTransactionsTotalValueByType(type: Type) {
+    const totalValue = transactions.reduce((acc, transaction) => { 
+      return transaction.type === type ? acc + transaction.value : acc
+    }, 0)
+
+    return totalValue
+  }
+
+  function formatCurrency(number: number) {
+    const formatedValue = new Intl.NumberFormat('pt-br', {
+      style: 'currency',
+      currency: 'BRL'
+    }).format(number)
+
+    return formatedValue
+  }
+
+  const income = calculateTransactionsTotalValueByType('income')
+  const outcome = calculateTransactionsTotalValueByType('outcome')
+
+  const total = income - outcome 
+
   return (
     <Container>
       <div>
         <header>
           <p>Entradas</p>
-          <img src={income} alt="Income" />
+          <img src={incomeImg} alt="Income" />
         </header>
-        <strong>R$15.000</strong>
+        <strong>{formatCurrency(income)}</strong>
       </div>
 
       <div>
         <header>
           <p>Saídas</p>
-          <img src={outcome} alt="Outcome" />
+          <img src={outcomeImg} alt="Outcome" />
         </header>
-        <strong>- R$5.000</strong>
+        <strong>- {formatCurrency(outcome)}</strong>
       </div>
 
       <div>
         <header>
           <p>Total</p>
-          <img src={total} alt="Total" />
+          <img src={totalImg} alt="Total" />
         </header>
-        <strong>R$10.000</strong>
+        <strong>{formatCurrency(total)}</strong>
       </div>
     </Container>
   )
